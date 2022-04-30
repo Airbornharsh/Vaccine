f418f4d56340f9587da81836d91371fcb405a671
Harsh Keshri <harshkeshri1234567@gmail.com>
2022-04-30 21:02:29 +0530

    Admin halff work done

--- admin.js
+++ admin.js
@@ -1,5 +1,7 @@
 class VaccineChart {
     constructor() {
+        this.template = document.querySelector("#useable");
+        this.tableBody = document.getElementById("tableBody");
         this.tabulation();
     }
 
@@ -7,6 +9,15 @@ class VaccineChart {
         let keylength = localStorage.length;
         for (let i = 0; i < keylength; i++) {
             let key = localStorage.key(i);
+            let copiedTemplate = this.template.cloneNode(true);
+            copiedTemplate.querySelector(".name").textContent = JSON.parse(window.localStorage.getItem(key)).name;
+            copiedTemplate.querySelector(".email").textContent = JSON.parse(window.localStorage.getItem(key)).email;
+            copiedTemplate.querySelector(".age").textContent = JSON.parse(window.localStorage.getItem(key)).age;
+            copiedTemplate.querySelector(".phone").textContent = JSON.parse(window.localStorage.getItem(key)).phone;
+            copiedTemplate.querySelector(".adhaar").textContent = JSON.parse(window.localStorage.getItem(key)).adhaarNumber;
+            copiedTemplate.querySelector(".address").textContent = JSON.parse(window.localStorage.getItem(key)).address;
+            copiedTemplate.querySelector(".date").textContent = JSON.parse(window.localStorage.getItem(key)).date;
+            this.tableBody.append(copiedTemplate);
         }
     }
 }
